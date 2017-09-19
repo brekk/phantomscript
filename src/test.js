cd1d38960b759823ae9940aed1382c11f0a0ab3d
brekk <brekk@brekkbockrath.com>
2017-09-18 22:13:23 -0700

    test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,13 @@
 import {length, filter, map, pipe} from 'f-utility'
 import {charCodeAt, magicRange} from '@fn/index'
 
+/**
+ * test whether a given input has zero-width characters in it
+ * @method test
+ * @param {string} whatever - a string or something
+ * @returns {boolean} whether the input has some zero-width characters in it
+ * @public
+ */
 export const test = pipe(
   Array.from,
   map(charCodeAt),
